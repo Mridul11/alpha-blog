e24d7814730713238b71640b5eb4f567cfe50dbd
Mridul11 <mridulmishra2991@gmail.com>
2018-04-10 13:00:01 +0530

    Styling elements

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    #@article = Article.find(params[:id]), use set_article method
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article is successfully created."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    #@article = Article.find(params[:id]), use set_article method
+    if @article.update(article_params)
+      flash[:notice] = "Article is successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    #@article = Article.find(params[:id]), use set_article method
+  end
+
+  def destroy
+    #@article = Article.find(params[:id]), use set_article method
+    @article.destroy
+      flash[:notice] = "Article successfully deleted."
+    redirect_to articles_path
+  end
+
+  private
+    def set_article
+      @article =Article.find(params[:id])
+    end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end

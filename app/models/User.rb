0e7175a188004b0453fd841c7c0e5ea23b18265b
Mridul11 <mridulmishra2991@gmail.com>
2018-04-11 21:23:20 +0530

    Added bcrypt and password one way encryption using bcrypt

--- app/models/User.rb
+++ app/models/User.rb
@@ -10,4 +10,6 @@ VALID_EMAIL_REGEX= /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     uniqueness: {case_sensitive:false},
                     format: {with: VALID_EMAIL_REGEX}
+
+  has_secure_password  
 end
